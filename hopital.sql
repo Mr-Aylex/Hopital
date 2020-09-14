85dcd5fa4b93430abc903acfef2e9256d062acdc
CARMONE Alexandre <carmonealexandre@hotmail.fr>
2020-09-14 15:45:04 +0200

    modification de la base de données

--- hopital.sql
+++ hopital.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 07 sep. 2020 à 13:52
+-- Généré le :  lun. 14 sep. 2020 à 13:41
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -31,15 +31,15 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `dossier_patients`;
 CREATE TABLE IF NOT EXISTS `dossier_patients` (
   `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nom` varchar(255) COLLATE utf8_bin NOT NULL,
-  `prenom` varchar(255) COLLATE utf8_bin NOT NULL,
+  `id_patient` int(10) UNSIGNED NOT NULL,
   `email` varchar(255) COLLATE utf8_bin NOT NULL,
   `adresse_post` varchar(255) COLLATE utf8_bin NOT NULL,
   `mutuelle` varchar(255) COLLATE utf8_bin NOT NULL,
   `num_ss` varchar(255) COLLATE utf8_bin NOT NULL,
   `opt` varchar(255) COLLATE utf8_bin NOT NULL,
   `regime` varchar(255) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_user_to_idpatient` (`id_patient`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -50,14 +50,15 @@ CREATE TABLE IF NOT EXISTS `dossier_patients` (
 
 DROP TABLE IF EXISTS `medecin`;
 CREATE TABLE IF NOT EXISTS `medecin` (
-  `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) COLLATE utf8_bin NOT NULL,
   `prenom` varchar(255) COLLATE utf8_bin NOT NULL,
-  `specialite` varchar(255) COLLATE utf8_bin NOT NULL,
+  `id_specialite` int(11) UNSIGNED NOT NULL,
   `email` varchar(255) COLLATE utf8_bin NOT NULL,
   `telephone` varchar(255) COLLATE utf8_bin NOT NULL,
   `lieu` varchar(255) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_spe_to_medecin` (`id_specialite`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -69,11 +70,12 @@ CREATE TABLE IF NOT EXISTS `medecin` (
 DROP TABLE IF EXISTS `rdv`;
 CREATE TABLE IF NOT EXISTS `rdv` (
   `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nom` varchar(255) COLLATE utf8_bin NOT NULL,
-  `prenom` varchar(255) COLLATE utf8_bin NOT NULL,
-  `email` varchar(255) COLLATE utf8_bin NOT NULL,
+  `id_patient` int(11) UNSIGNED NOT NULL,
+  `id_medecin` int(11) UNSIGNED NOT NULL,
   `date_rdv` date NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_user_to_idpatien` (`id_patient`),
+  KEY `fk_medecin_to_idmedecin` (`id_medecin`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -84,10 +86,10 @@ CREATE TABLE IF NOT EXISTS `rdv` (
 
 DROP TABLE IF EXISTS `specialites`;
 CREATE TABLE IF NOT EXISTS `specialites` (
-  `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -97,14 +99,30 @@ CREATE TABLE IF NOT EXISTS `specialites` (
 
 DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `id` smallint(255) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) COLLATE utf8_bin NOT NULL,
   `prenom` varchar(255) COLLATE utf8_bin NOT NULL,
   `email` varchar(255) COLLATE utf8_bin NOT NULL,
-  `pass` varchar(255) COLLATE utf8_bin NOT NULL,
+  `mdp` varchar(255) COLLATE utf8_bin NOT NULL,
   `role` varchar(255) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `medecin`
+--
+ALTER TABLE `medecin`
+  ADD CONSTRAINT `fk_spe_to_medecin` FOREIGN KEY (`id_specialite`) REFERENCES `specialites` (`id`);
+
+--
+-- Contraintes pour la table `rdv`
+--
+ALTER TABLE `rdv`
+  ADD CONSTRAINT `fk_test` FOREIGN KEY (`id_medecin`) REFERENCES `medecin` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

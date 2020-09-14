6c6feaebba83f80d10f32c248901d8b8eb0b7c8d
CARMONE Alexandre <carmonealexandre@hotmail.fr>
2020-09-14 15:19:48 +0200

    entity user terminer

--- back/entity/user.php
+++ back/entity/user.php
@@ -1,7 +1,133 @@
 <?php
-
+/**
+ * Class user
+ */
 
 class user
 {
+    /**
+     * @var string
+     */
+    protected $nom;
+    /**
+     * @var string
+     */
+    protected $prenom;
+    /**
+     * @var string
+     */
+    protected $mail;
+    /**
+     * @var string
+     */
+    protected $mdp;
+    /**
+     * @var string
+     */
+    protected $role;
+
+    /**
+     * user constructor.
+     * @param array $array
+     */
+    public function __construct($array)
+    {
+        $this->hydrate($array);
+    }
+    /**
+     * @param array $donnees
+     */
+    public function hydrate($donnees)
+    {
+        foreach($donnees as $key => $value){
+            $method = 'set'.ucfirst($key);
+            if(method_exists($this,$method)){
+                $this->$method($value);
+            }
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param string $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param string $prenom
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * @param string $mail
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMdp()
+    {
+        return $this->mdp;
+    }
+
+    /**
+     * @param string $mdp
+     */
+    public function setMdp($mdp)
+    {
+        $this->mdp = $mdp;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param string $role
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+    }
+
 
 }
+?>

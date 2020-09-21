a56ba2f9e9df15a9c476627b739d45d2685994b2
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2020-09-21 16:52:48 +0200

    Correction traitement backend

--- back/inscription_backend.php
+++ back/inscription_backend.php
@@ -5,7 +5,7 @@ session_start();
 require_once($_SERVER['DOCUMENT_ROOT'].'Hopital/back/entity/user.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'Hopital/back/manager/manager.php');
 
-$new_user = new user((
+$new_user = new user(array(
   'nom' => $_POST['nom'],
   'prenom' => $_POST['prenom'],
   'email' => $_POST['email'],

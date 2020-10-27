11b049549ff3a7c17fdbaac1dc67861ee8ecc7ba
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2020-10-27 11:44:45 +0100

    Prise de rdv en ligne

--- web/rdv.php
+++ web/rdv.php
@@ -5,48 +5,91 @@
 include '../include/header.php';
 ?>
 
+<script language="JavaScript">
+function verif()
+  {
+  if (document.layers)
+    {
+    formulaire = document.forms.monRDV;
+    }
+  else
+    {
+    formulaire = document.monRDV;
+    }
+  }
+
+function verifChoixSpe()
+  {
+  verif();
+  if (formulaire.specialites.value == "0")
+    {
+    alert('Vous devez choisir une spécialité !');
+    formulaire.specialites.focus();
+    }
+  }
+
+var motifs = new Array();
+motifs[0] = new Array();
+motifs[1] = new Array("Première consultation", "Consultation de suivi")
+motifs[2] = new Array("Première consultation", "Consultation de suivi")
+motifs[3] = new Array("Première consultation", "Consultation de suivi")
+motifs[4] = new Array("Première consultation", "Consultation de suivi")
+motifs[5] = new Array("Première consultation", "Consultation de suivi")
+
+function remplirMotifs(code)
+  {
+  verif();
+  var lesMotifs = motifs[code];
+
+  if (code>0)
+    {
+    formulaire.choixMotif.options.length = lesMotifs.length;
+    for (i=0; i<lesMotifs.length; i++)
+      {
+      formulaire.choixMotif.options[i].value = lesMotifs[i];
+      formulaire.choixMotif.options[i].text = lesMotifs[i];
+      }
+    document.monRdv.choixMotif.options.selectedIndex = 0;
+    }
+  else
+    {
+    formulaire.choixMotif.options.length = 1;
+    formulaire.choixMotif.options[0].value = 0;
+    formulaire.choixMotif.options[0].text = "--Quel est votre motif ?--";
+    }
+  }
+</script>
+
 <div class="hero" style="background-image: url('/Hopital/images/hero_bg_1.jpg');">
   <div class="container" style="padding-top: 150px">
-      <form action="" method="post">
-          <div class="form-group row">
-              <div class="col-md-12">
-                  <input type="text" class="form-control" name="lieu" placeholder="Où ?" required>
-              </div>
-          </div>
+      <form name="monRdv" action="../back/.php" method="post">
+        <div class="form-group row">
+            <div class="col-md-12">
+              <select name="specialites" onChange="remplirMotifs(this.options[this.selectedIndex].value);">>
+                <option value="0" selected>--Choisir une spécialité--</option>
+                <option value="1">Addictologue</option>
+                <option value="2">Infirmier</option>
+                <option value="3">Hématologue</option>
+                <option value="4">Hépatologue</option>
+                <option value="5">Tabacologue</option>
+              </select>
+            </div>
+        </div>
 
           <div class="form-group row">
               <div class="col-md-12">
-                <label for="motif-select"></label>
-                <select name="motif" id="motif-select">
-                  <option value="">Quel est votre motif ?</option>
-                  <option value=""></option>
-                  <option value=""></option>
-                  <option value=""></option>
+                <select name="choixMotif" onFocus="verifChoixSpe();">
+                  <option value="0" selected>--Quel est votre motif ?--</option>
                 </select>
               </div>
           </div>
 
           <div class="form-group row">
               <div class="col-md-12">
-                <label for="specialites-select"></label>
-                <select name="specialites" id="specialites-select">
-                  <option value="">Choisir une spécialité</option>
-                  <option value=""></option>
-                  <option value=""></option>
-                  <option value=""></option>
-                  <option value=""></option>
-                  <option value=""></option>
-                  <option value=""></option>
-                </select>
+                <input type="hidden" value="<?php echo $_POST['']?>" name="id_medecin">
               </div>
           </div>
 
-          <div class="form-group row">
-              <div class="col-md-12">
-                <label for="heure_rdv">Choose a time for your meeting:</label>
-                  <input type="time" class="form-control" name="heure_rdv" min="8:00" max="19:00" required>
-              </div>
-          </div>
 
           <div class="form-group row">
               <div class="col-md-6 mr-auto">

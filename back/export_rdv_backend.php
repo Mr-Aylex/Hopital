da53453aef38dae2a4057e99d513140c5cd4a437
MinhNguyenTang <minhnguyyen7@gmail.com>
2020-11-23 15:13:17 +0100

    Correction traitement

--- back/export_rdv_backend.php
+++ back/export_rdv_backend.php
@@ -9,6 +9,6 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/Hopital/back/entity/user.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/Hopital/back/manager/manager.php');
 $export_rdv = new dossier($_POST);
 $manager = new manager();
-$manager->export_dossier($export_rdv);
+$manager->export_rdv($export_rdv);
 
  ?>
